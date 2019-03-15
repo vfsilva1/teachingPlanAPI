2d637a5119180bd865a5cd41f98bfeca429e836e
Rafael Santana <rafaellauton@outlook.com>
2019-03-15 18:13:35 -0300

    Projeto Criado

---
+++ planner/src/main/java/edu/planner/TeachingPlanApplication.java
@@ -0,0 +1,13 @@
+package edu.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingPlanApplication.class, args);
+	}
+
+}
